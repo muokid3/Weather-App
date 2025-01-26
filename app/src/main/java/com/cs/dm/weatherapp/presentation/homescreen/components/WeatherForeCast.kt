6b067a69bf7f92903d9d8fe7029bad1a87ed51ce
Dennis Muoki <muokid3@gmail.com>
2025-01-26 09:33:56 +0300

    more theming and styling

--- app/src/main/java/com/cs/dm/weatherapp/presentation/homescreen/components/WeatherForeCast.kt
+++ app/src/main/java/com/cs/dm/weatherapp/presentation/homescreen/components/WeatherForeCast.kt
@@ -1,6 +1,8 @@
 package com.cs.dm.weatherapp.presentation.homescreen.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,12 +10,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cs.dm.weatherapp.presentation.homescreen.WeatherState
@@ -26,37 +35,54 @@ fun WeatherForecast(
     modifier: Modifier = Modifier
 ) {
     state.forecastData?.let { map ->
-        map.entries.forEach { items ->
+
+        Text(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            textAlign = TextAlign.End,
+            textDecoration = TextDecoration.Underline,
+            text = "Next 5 days forecast",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        map.entries.forEachIndexed { index, items ->
             Column(
                 modifier = modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             ) {
                 Text(
-                    text = LocalDate.parse(items.key, DateTimeFormatter.ofPattern("yyyy-MM-dd")).format(DateTimeFormatter.ofPattern("EEE dd MMM")),
+                    text = if (index == 0) {
+                        "Today - "
+                    } else {
+                        ""
+                    } + LocalDate.parse(items.key, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                        .format(DateTimeFormatter.ofPattern("EEE dd MMM")),
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
-                    color = Color.White
+                    //color = Color.White
                 )
                 Spacer(modifier = Modifier.height(8.dp))
-                LazyRow(content = {
-                    items(items.value) { weatherData ->
-                        HourlyWeatherDisplay(
-                            weatherData = weatherData,
-                            modifier = Modifier
-                                .height(100.dp)
-                                .padding(horizontal = 16.dp)
+                LazyRow(
+                    horizontalArrangement = Arrangement.spacedBy(5.dp),
+                    content = {
+                        items(items.value) { weatherData ->
+                            HourlyWeatherDisplay(
+                                weatherData = weatherData,
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(5.dp))
+                                    .background(MaterialTheme.colorScheme.surfaceContainer)
+                                    .padding(horizontal = 16.dp, vertical = 4.dp)
+                                    .height(100.dp)
 
-                        )
-                    }
-                })
+                            )
+                        }
+                    })
                 Spacer(modifier = Modifier.height(8.dp))
                 HorizontalDivider()
                 Spacer(modifier = Modifier.height(8.dp))
 
             }
         }
-
     }
-
 }
